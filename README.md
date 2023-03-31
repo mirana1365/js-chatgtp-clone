e4cfe54d782678f0e8387560cd33f9b0fcb298dd
Ali Rowshani <31807283+mirana1365@users.noreply.github.com>
2023-03-31 01:24:05 +0000

    COMMIT AND PUSH ALL FILE ON THE GITHUB

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # js-chatgtp-clone
 this is a chatgpt clone with javascript
+# Name : GeekyGPT
+Project Title : Chat GPT Clone With Javascript (CSS - HTML) 
+Auther : Armin(Ali) Rowshani
+Description : ChatGPT has quickly become one of the most significant tech launches since the original Apple iPhone in 2007. The chatbot is now the fastest-growing consumer app in history, hitting 100 million users in only two months – but it's also a rapidly-changing AI shapeshifter, which can make it confusing and overwhelming.
+Version : 1.0
---
+++ app.js
@@ -0,0 +1,52 @@
+// Dont deploy This APi KEY or upload on to github
+const API_KEY = 'YOUR-API-KEY';
+
+const submitButton   = document.querySelector('#submit');
+const outPutElement  = document.querySelector('#result');
+const inputElement   = document.querySelector('input');
+const historyElement = document.querySelector('.chat-history');
+const buttonElement  = document.querySelector('button');
+
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input');
+    inputElement.value = value;
+}
+
+async function getMessage() {
+        console.log('Success!');
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+                'Content-Type' : 'application/json'
+            }, 
+            body : JSON.stringify({
+                model : "gpt-3.5-turbo",
+                messages: [{role: "user", content: "Hello!"}],
+                max_tokens: 100
+            })
+        }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+        const data = await response.clone().json();
+        console.log(data);
+        outPutElement.textContent = data.choices[0].message.content;
+            if (data.choices[0].message.content && inputElement.value) {
+                const pElement = document.createElement('p');
+                pElement.textContent = inputElement.value;
+                pElement.addEventListener('click', () => changeInput());
+                historyElement.append(pElement);
+            }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+submitButton.addEventListener('click', getMessage);
+
+function clearInput() {
+    inputElement.value = '';
+}
+
+buttonElement.addEventListener('click', clearInput);
